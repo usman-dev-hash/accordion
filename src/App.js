73c7646459408886bccab8df691e1aced6e1058c
usman@carswitch.com <usman@carswitch.com>
2022-08-03 04:02:51 +0500

    finalized commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, {useState} from 'react';
+import data from './data.js';
+import Question from './Question.js';
+
+function App(){
+
+    const [questions, setQuestions] = useState(data);
+
+    return <main>
+        <div className='container'>
+            <h3>questions & answers about login</h3>
+            <section className='info'>
+                {questions.map( (question) => {
+                    return <Question key={question.id} {...question} />
+                })}
+            </section>
+        </div>
+    </main>
+
+}
+
+export default App;
